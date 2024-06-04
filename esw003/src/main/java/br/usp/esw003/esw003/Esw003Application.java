5ac3dcb2c0c8c5058f7677b4367ba789477543da
Victor Souza <victor.souza2@gmail.com>
2024-06-04 19:09:54 -0300

    spring boot initial application

---
+++ esw003/src/main/java/br/usp/esw003/esw003/Esw003Application.java
@@ -0,0 +1,13 @@
+package br.usp.esw003.esw003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esw003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esw003Application.class, args);
+	}
+
+}
